e75de9361530d268dd5214fdd1317e9e7e0ff5cd
Jagadeesh-Narayanam <121303235+Jagadeesh-Narayanam@users.noreply.github.com>
2023-02-13 15:38:22 +0530

    Delete frontend directory

--- frontend/src/pages/Success.js
+++
@@ -1,9 +0,0 @@
-function Success() {
-  return (
-    <>
-      <h1 className="heading">Registration Request sent</h1>
-      <h4 className="heading">You will be able to login once the admin approves your request</h4>
-    </>
-  );
-}
-export default Success;
--- frontend/src/util/Logout.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export function logoutAction(){
-    localStorage.removeItem("token");
-    localStorage.removeItem("username");
-    return redirect("/")
-}
